cec59b8d05cfe280f69f4f16ef27dd061fa216d7
ankitagupta15 <gankita009@gmail.com>
2022-08-24 23:29:06 +0530

    px change

--- login-page.js
+++ login-page.js
@@ -1,9 +1,10 @@
 //astrinsic px code
-  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-RDSEZXDYZ5TP-1");
+(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+  (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-9DYNKKOX8BGS-1");
+
 
 //feedback function
 function feedback(){
