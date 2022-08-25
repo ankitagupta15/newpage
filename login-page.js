1ccef45c8cf79280d7ca04e24fa517c90ce20de6
ankitagupta15 <gankita009@gmail.com>
2022-08-25 15:13:54 +0530

    change tag

--- login-page.js
+++ login-page.js
@@ -3,7 +3,7 @@
   (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
 var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
 var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-9DYNKKOX8BGS-1",{fullDomainCookie: true});
+})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-RDSEZXDYZ5TP-1",{fullDomainCookie: true});
 
 //feedback function
 function feedback(){
